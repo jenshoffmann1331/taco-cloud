cbe8adc4d7a5dc76f2a95af96189c75b15a9f33c
Jens Hoffmann <jens.hoffmann@otto.de>
2022-11-22 10:45:20 +0100

    Add taco design controller and view

---
+++ src/main/java/fullstacklab/tacocloud/domain/Taco.java
@@ -0,0 +1,11 @@
+package fullstacklab.tacocloud.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<String> ingredients;
+}
